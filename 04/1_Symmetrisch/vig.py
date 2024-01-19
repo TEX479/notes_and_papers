ae8afe9c6032d8707a356f9175854319af167e9b
TEX_479 <109556530+TEX479@users.noreply.github.com>
2024-01-19 22:08:20 +0100

    shortened

--- 04/1_Symmetrisch/vig.py
+++ 04/1_Symmetrisch/vig.py
@@ -1,2 +1,2 @@
-def vig(text:str, key:str):
-    return"".join([chr((((ord(text[i])&31)+(ord(key[i%len(key)])&31)-2)%26)+65+(ord(text[i])&32))if ord(text[i])in(list(range(65,91))+list(range(97,123)))else text[i]for(i)in range(len(text))])
+def vig(t,k):
+    return"".join([chr((((ord(t[i])&31)+(ord(k[i%len(k)])&31)-2)%26)+65+(ord(t[i])&32))if ord(t[i])in(list(range(65,91))+list(range(97,123)))else t[i]for(i)in range(len(t))])
